eefaf29063f35ac9aec0cc4269e6ff237694bfc6
shubham24x7 <androidshubham@gmail.com>
2018-12-30 14:10:02 +0530

    initial start with React

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, {  Component} from 'react';
+// import logo from './logo.svg';
+import './App.css';
+import {  BrowserRouter as Router,  Route} from "react-router-dom";
+import LoginComponent from './components/login';
+
+class App extends Component {
+  render() {
+    return ( 
+      <div>
+        <Router>
+          <Route path = "/login" component = { LoginComponent } /> 
+        </Router>
+      </div>
+
+    );
+  }
+}
+
+export default App;
